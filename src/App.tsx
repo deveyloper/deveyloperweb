d3bb39f0d0e4d647e9c095f0e2583f32f6e0d5dd
conqsolid <conqsolid@gmail.com>
2019-11-10 01:27:34 +0300

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import './assets/site.scss';
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="container">
     </div>
   );
 }
